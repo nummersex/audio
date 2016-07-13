bf28f738163a7867115f39bdb895cae3dbb30b11
Gabriel Englund <gabriel.englund@assaabloy.com>
2016-07-13 09:35:30 +0200

    Changed the IAudioProgram interface to be an abstract class instead

---
+++ Audio/AudioProgram.cs
@@ -0,0 +1,24 @@
+﻿namespace Audio
+{
+    public abstract class AudioProgram
+    {
+        protected AudioProgram(string originalFileName,
+            string newFileName,
+            IEncoder encoder)
+        {
+            OriginalFileName = originalFileName;
+            NewFileName = newFileName;
+            Encoder = encoder;
+        }
+
+        public string OriginalFileName { get; set; }
+
+        public string NewFileName { get; set; }    
+    
+        public IEncoder Encoder { get; set; }
+
+        public virtual void Start()
+        {
+        }
+    }
+}
